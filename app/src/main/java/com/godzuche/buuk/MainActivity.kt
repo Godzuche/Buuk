fbbb3f3d5c07a8ca515225913d4af85f9f4bd323
Administrator <godxuche@gmail.com>
2021-12-12 01:48:34 +0100

    successfully migrated to using one Activity

--- app/src/main/java/com/godzuche/buuk/MainActivity.kt
+++ app/src/main/java/com/godzuche/buuk/MainActivity.kt
@@ -2,25 +2,61 @@ package com.godzuche.buuk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.Transition
+import android.view.Gravity
+import android.view.View
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.isGone
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.transition.Slide
+import androidx.transition.TransitionManager
 import com.godzuche.buuk.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
+        installSplashScreen()
+
         setContentView(view)
 
         setSupportActionBar(binding.appBarMain.toolbarMain)
+
+        bottomNavigationView = binding.bottomNavView
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
         setupActionBarWithNavController(navController)
+        bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.loginFragment -> hideTopAppBarAndBottomNav()
+                else -> showTopAppBarAndBottomNav()
+            }
+        }
+    }
+
+    private fun showTopAppBarAndBottomNav() {
+        binding.appBarMain.toolbarMain.visibility = View.VISIBLE
+        bottomNavigationView.visibility = View.VISIBLE
+    }
+
+    private fun hideTopAppBarAndBottomNav() {
+        TransitionManager.beginDelayedTransition(binding.root, Slide(Gravity.BOTTOM).excludeTarget(R.id.nav_host_fragment, true))
+        binding.appBarMain.toolbarMain.visibility = View.GONE
+        bottomNavigationView.visibility = View.GONE
     }
 }
--- app/src/main/java/com/godzuche/buuk/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/godzuche/buuk/ui/auth/LoginFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.godzuche.buuk.R
+import com.google.android.material.appbar.MaterialToolbar
 
 
 class LoginFragment : Fragment() {
