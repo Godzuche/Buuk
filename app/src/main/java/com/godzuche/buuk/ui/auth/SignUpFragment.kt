cd76c8206fc7f0d3ab2adf45c6d716722cbcb8b4
Administrator <godxuche@gmail.com>
2021-12-13 14:32:46 +0100

    successfully implemented the signup functionality using email, fullname and password...

--- app/src/main/java/com/godzuche/buuk/ui/auth/SignUpFragment.kt
+++ app/src/main/java/com/godzuche/buuk/ui/auth/SignUpFragment.kt
@@ -1,16 +1,15 @@
 package com.godzuche.buuk.ui.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.navigation.findNavController
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.godzuche.buuk.R
 import com.godzuche.buuk.databinding.FragmentSignUpBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.CoroutineScope
@@ -41,7 +40,10 @@ class SignUpFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         auth = Firebase.auth
         binding.apply {
-            btCreateNewAccount.setOnClickListener { registerUser() }
+            btCreateNewAccount.setOnClickListener {
+                registerUser()
+//                updateProfileUserName()
+            }
         }
     }
 
@@ -57,14 +59,48 @@ class SignUpFragment : Fragment() {
         findNavController().navigate(SignUpFragmentDirections.actionSignUpFragmentToActionLearn())
     }
 
+    /*private fun updateProfileUserName() {
+        val username = binding.etName.text.toString()
+        auth.currentUser?.let { user ->
+//            val photoUri =
+            val profileUpdate = UserProfileChangeRequest.Builder()
+                .setDisplayName(username)
+//                .setPhotoUri()
+                .build()
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    user.updateProfile(profileUpdate).await()
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(requireContext(),
+                            "Successfully  updated user profile",
+                            Toast.LENGTH_LONG).show()
+                        goToHomeFragment()
+
+                    }
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }*/
+
     private fun registerUser() {
         val email = binding.etEmail.text.toString()
         val password = binding.etPassword.text.toString()
-        if (email.isNotEmpty() && password.isNotEmpty()) {
+        val username = binding.etName.text.toString()
+        val profileUpdate = UserProfileChangeRequest.Builder()
+            .setDisplayName(username)
+//                .setPhotoUri()
+            .build()
+        if (email.isNotEmpty() && password.isNotEmpty() && username.isNotEmpty()) {
             CoroutineScope(Dispatchers.IO).launch {
                 try {
                     auth.createUserWithEmailAndPassword(email, password).await()
-                    withContext(Dispatchers.Main) {
+                    Firebase.auth.currentUser?.updateProfile(profileUpdate)?.await()
+
+                        withContext(Dispatchers.Main) {
                         checkLoggedInState()
                     }
                 } catch (e: Exception) {
@@ -79,8 +115,11 @@ class SignUpFragment : Fragment() {
     private fun checkLoggedInState() {
         if (auth.currentUser == null) {
             Toast.makeText(requireContext(), "You are not logged in", Toast.LENGTH_LONG).show()
-        } else {
+        } else if (auth.currentUser != null && auth.currentUser!!.displayName != null){
             Toast.makeText(requireContext(), "You are now logged in", Toast.LENGTH_LONG).show()
+            Toast.makeText(requireContext(),
+                "Successfully  updated user profile",
+                Toast.LENGTH_LONG).show()
             goToHomeFragment()
         }
     }
--- app/src/main/java/com/godzuche/buuk/ui/main/CoursesFragment.kt
+++ app/src/main/java/com/godzuche/buuk/ui/main/CoursesFragment.kt
@@ -3,10 +3,14 @@ package com.godzuche.buuk.ui.main
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.godzuche.buuk.R
 import com.godzuche.buuk.databinding.FragmentCoursesBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class CoursesFragment : Fragment() {
@@ -25,6 +29,11 @@ class CoursesFragment : Fragment() {
         binding = FragmentCoursesBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val t = binding.t
+        t.text = Firebase.auth.currentUser?.displayName
     }
 
 }
